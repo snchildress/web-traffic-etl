2dafcf889d57733e753ac9623fc93cef77040524
Steven Childress <steven.n.childress@gmail.com>
2021-04-02 18:07:46 -0500

    Generate list of file names

--- src/services.py
+++ src/services.py
@@ -1,3 +1,6 @@
+import string 
+
+
 class ExtractionService:
     @classmethod
     def generate_file_names(self):
@@ -7,7 +10,4 @@ class ExtractionService:
         returns:
             [str]: the list of web traffic data file names
         """
-        # TODO: generate list dynamically
-        return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        return list(string.ascii_lowercase)
