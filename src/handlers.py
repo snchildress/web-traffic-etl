a4fc33ea1ff18e1caa3dd790ae1106e6ec07312c
Steven Childress <steven.n.childress@gmail.com>
2021-04-03 13:29:52 -0500

    Add transform handler

--- src/handlers.py
+++ src/handlers.py
@@ -1,3 +1,4 @@
+from .exceptions import InvalidParams
 from .services import ExtractionService
 
 
@@ -20,3 +21,70 @@ class ExtractionHandler:
         file_names = ExtractionService.generate_file_names()
         return [ExtractionService.fetch_csv_rows(file_name) for file_name
                 in file_names]
+
+
+class TransformationHandler:
+    @classmethod
+    def transform(self, rows: list[list[str]]) \
+            -> dict[int, dict[str, list[int]]]:
+        """
+        placeholder
+        """
+        parsed_rows = [self.parse_row(row) for row in rows]
+        return self.sort_rows_by_user_id(parsed_rows)
+
+    class Row:
+        user_id: int
+        path: str
+        length: int
+
+        def __init__(self, user_id, path, length):
+            self.user_id = user_id
+            self.path = path
+            self.length = length
+
+    @ classmethod
+    def parse_row(self, row: list[str]) -> Row:
+        """
+        returns an instance of the Row class
+        for a given CSV row
+
+        row  list[str]: a CSV row, where user ID and
+                        length can be parsed as integers
+
+        returns:
+            Row()
+        """
+        try:
+            user_id = int(row[4])
+            path = row[2]
+            length = int(row[1])
+        except (TypeError, ValueError, IndexError, KeyError):
+            raise InvalidParams()
+        return self.Row(user_id=user_id, path=path, length=length)
+
+    @ classmethod
+    def sort_rows_by_user_id(self, rows: Row) -> \
+            dict[int, dict[str, list[int]]]:
+        """
+        placholder
+        """
+        sorted_rows = {}
+
+        for row in rows:
+            user_id = row.user_id
+            path = row.path
+            length = row.length
+
+            if user_id not in sorted_rows:
+                sorted_rows[user_id] = {
+                    path: length
+                }
+            else:
+                user = sorted_rows[user_id]
+                if path in user:
+                    user[path] += length
+                else:
+                    user[path] = length
+
+        return sorted_rows
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,7 +1,8 @@
 import unittest
 from unittest.mock import patch, call
 
-from src.handlers import ExtractionHandler
+from src.exceptions import InvalidParams
+from src.handlers import ExtractionHandler, TransformationHandler
 
 
 class TestExtractionHandler(unittest.TestCase):
@@ -30,3 +31,100 @@ class TestExtractionHandler(unittest.TestCase):
         generate_file_names_mock.assert_called_once_with()
         fetch_csv_rows_mock.assert_has_calls(expected_fetch_csv_rows_calls)
         self.assertEqual(actual_csv_rows, expected_csv_rows)
+
+
+class TestTransformationHandler(unittest.TestCase):
+    def test_parse_row(self):
+        """
+        Tests that a CSV row is successfully parsed into a Row instance
+        """
+        expected_row = TransformationHandler.Row(
+            user_id=378,
+            path='/',
+            length=7
+        )
+        actual_row = TransformationHandler.parse_row([
+            '1',
+            '7',
+            '/',
+            'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0\t',  # noqa: E501
+            '378'
+        ])
+
+        self.assertEqual(actual_row.user_id, expected_row.user_id)
+        self.assertEqual(actual_row.path, expected_row.path)
+        self.assertEqual(actual_row.length, expected_row.length)
+
+    def test_parse_invalid_params(self):
+        """
+        Tests that an invalid CSV row is handled correctly
+        """
+        for invalid_row in [['1'], 1, '', None, ['a', 'a', 'a', 'a', 'a'],
+                            ['', '', '', '', ''], {'a': 'a'}]:
+            with self.assertRaises(InvalidParams):
+                TransformationHandler.parse_row(invalid_row)
+
+    def test_sort_rows_by_user_id(self):
+        """
+        Tests that Row instances are successfully sorted by user ID
+        """
+        expected_sorted_rows = {
+            1: {
+                '/': 10,
+                '/test': 5
+            },
+            2: {
+                '/': 1
+            },
+            3: {
+                '/help': 12
+            }
+        }
+        test_rows = [
+            TransformationHandler.Row(user_id=1, path='/', length=2),
+            TransformationHandler.Row(user_id=1, path='/', length=5),
+            TransformationHandler.Row(user_id=1, path='/test', length=5),
+            TransformationHandler.Row(user_id=2, path='/', length=1),
+            TransformationHandler.Row(user_id=3, path='/help', length=2),
+            TransformationHandler.Row(user_id=3, path='/help', length=2),
+            TransformationHandler.Row(user_id=3, path='/help', length=8),
+            TransformationHandler.Row(user_id=1, path='/', length=3),
+        ]
+
+        actual_sorted_rows = TransformationHandler.sort_rows_by_user_id(
+            test_rows
+        )
+
+        self.assertEqual(actual_sorted_rows, expected_sorted_rows)
+
+    def test_transform(self):
+        """
+        Tests that CSV rows are transformed into a dictionary of
+        user IDs and their web traffic activity
+        """
+        expected_sorted_rows = {
+            1: {
+                '/': 10,
+                '/test': 5
+            },
+            2: {
+                '/': 1
+            },
+            3: {
+                '/help': 12
+            }
+        }
+        test_rows = [
+            ['1', '2', '/', '', '1'],
+            ['0', '5', '/', '', '1'],
+            ['0', '5', '/test', '', '1'],
+            ['0', '1', '/', '', '2'],
+            ['0', '2', '/help', '', '3'],
+            ['0', '2', '/help', '', '3'],
+            ['1', '8', '/help', '', '3'],
+            ['0', '3', '/', '', '1'],
+        ]
+
+        actual_sorted_rows = TransformationHandler.transform(test_rows)
+
+        self.assertEqual(actual_sorted_rows, expected_sorted_rows)
